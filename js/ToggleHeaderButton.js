fd62971002f1c2b3904dd61b868074c89cb55d4e
SolaireFR <jesappellegrootdu59@gmail.com>
2023-03-08 16:11:43 +0100

    fin header

---
+++ js/ToggleHeaderButton.js
@@ -0,0 +1,41 @@
+export default class ToogleHeaderButton{
+	openHeader;
+	closeHeader;
+	navBar;
+
+	constructor(header){
+		this.openHeader = header.querySelector(".openHeader");
+		this.closeHeader = header.querySelector(".closeHeader");
+		this.navBar = header.querySelector(".navBar");
+
+		// Situation initiale
+		this.hideNavBar();
+
+		// Ajout des evenement
+		this.openHeader.addEventListener('click', event => {event.preventDefault();this.showNavBar()});
+		this.closeHeader.addEventListener('click', event => {event.preventDefault();this.hideNavBar()});
+		window.addEventListener('resize', event=>{this.windowGrow()});
+	}
+
+	windowGrow(){
+		if (window.innerWidth > 919) {
+			this.showNavBar();
+			this.closeHeader.classList.add('hide');
+		}
+		else{
+			this.hideNavBar();
+		}
+	}
+
+	showNavBar(){
+		this.openHeader.classList.add('hide');
+		this.closeHeader.classList.remove('hide');
+		this.navBar.classList.remove('hide');
+	}
+
+	hideNavBar(){
+		this.openHeader.classList.remove('hide');
+		this.closeHeader.classList.add('hide');
+		this.navBar.classList.add('hide');
+	}
+}
--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
 import ProjetListView from './ProjetListView.js';
-import toggleHeader from './toggleHeader.js';
+import ToogleHeaderButton from './ToggleHeaderButton.js';
 
-const toggleHeaderButton = document.querySelectorAll(
-	'.openHeader, .closeHeader'
-);
-toggleHeaderButton.forEach(button =>
-	button.addEventListener('click', toggleHeader)
-);
+/* INIT */
+const header = document.querySelector('body>header');
+const toogleHeaderButton = new ToogleHeaderButton(header);
 
-window.addEventListener('resize', WindowGrow);
-
-function WindowGrow(event) {
-	if (window.innerWidth > 919) {
-		document.querySelectorAll('.openHeader, .closeHeader').forEach(button => {
-			button.classList.remove('show');
-			button.classList.add('hide');
-		});
-	} else {
-		document.querySelectorAll('.openHeader').forEach(button => {
-			button.classList.remove('hide');
-			button.classList.add('show');
-		});
-		document.querySelectorAll('.closeHeader').forEach(button => {
-			button.classList.remove('show');
-			button.classList.add('hide');
-		});
-	}
-}
+/* NEXT */
 
 //const projectListView = new ProjetListView(document.querySelector('#projets'));
 
